b6d64f7ee35acd6dbafbdf9408bee29fce10a634
Vít Ungermann <ungermann.vit@gmail.com>
2025-01-21 20:05:46 +0100

    pridal jsem semafor

--- simulace/simulace/Program.cs
+++ simulace/simulace/Program.cs
@@ -13,6 +13,8 @@
             private List<Car> Arsenal { get; set; }
             private int Sand { get; set; }
             private bool isLoading { get; set; }
+            
+            private int SemaforInterval { get; set; }
             public void Start()
             {
                 EventQueue = new PriorityQueue<Event, int>();
@@ -38,6 +40,19 @@
                     Console.WriteLine(EventQueue.Dequeue());
                 }
             }
+
+            private int CalculateSemafor(int t)
+            {
+                int closestPastSemaforEvent = t / SemaforInterval * SemaforInterval;
+                bool redOrGreen = (t / SemaforInterval) % 2 == 1;
+
+                if (redOrGreen)
+                {
+                    return -1;
+                }
+
+                return closestPastSemaforEvent + SemaforInterval - t;
+            }
             private void ProcessEvent(Event e)
             {
                 Console.WriteLine(e);
@@ -46,13 +61,42 @@
                 {
                     Sand -= e.Car.Capacity;
                     isLoading = true;
+                    
+                    int casPrujezduSemaforemTam = e.Time + e.Car.LoadingTime + e.Car.TravelTime / 2;
+                    int semaforDelay = CalculateSemafor(casPrujezduSemaforemTam);
+                    if (semaforDelay == -1)
+                    {
+                        EventQueue.Enqueue(new Event(e.Car ,EventType.SEMAFOR, Status.PRUJEZD, casPrujezduSemaforemTam), casPrujezduSemaforemTam);
+                        semaforDelay = 0;
+                    }
+                    else
+                    {
+                        EventQueue.Enqueue(new Event(e.Car ,EventType.SEMAFOR, Status.STOP, casPrujezduSemaforemTam), casPrujezduSemaforemTam);
+                        EventQueue.Enqueue(new Event(e.Car ,EventType.SEMAFOR, Status.VYRAZIT, casPrujezduSemaforemTam + semaforDelay), casPrujezduSemaforemTam + semaforDelay);
+                    }
+
+                    int casPrujezduSemaforemZpet = e.Time + e.Car.LoadingTime + e.Car.TravelTime + e.Car.UnloadingTime + semaforDelay + e.Car.TravelTime / 2;
+                    int semaforDelayZpet = CalculateSemafor(casPrujezduSemaforemZpet);
+
+                    if (semaforDelayZpet == -1)
+                    {
+                        EventQueue.Enqueue(new Event(e.Car ,EventType.SEMAFOR, Status.PRUJEZD, casPrujezduSemaforemZpet), casPrujezduSemaforemZpet);
+                        semaforDelayZpet = 0;
+                    }
+                    else
+                    {
+                        EventQueue.Enqueue(new Event(e.Car ,EventType.SEMAFOR, Status.STOP, casPrujezduSemaforemZpet), casPrujezduSemaforemZpet);
+                        EventQueue.Enqueue(new Event(e.Car ,EventType.SEMAFOR, Status.VYRAZIT, casPrujezduSemaforemZpet + semaforDelayZpet), casPrujezduSemaforemZpet + semaforDelayZpet);
+                    }
+                    
                     EventQueue.Enqueue(new Event(e.Car ,EventType.LOAD, Status.FINISH, e.Time + e.Car.LoadingTime), e.Time + e.Car.LoadingTime);
                     EventQueue.Enqueue(new Event(e.Car ,EventType.TRAVEL, Status.START, e.Time + e.Car.LoadingTime), e.Time + e.Car.LoadingTime);
-                    EventQueue.Enqueue(new Event(e.Car ,EventType.TRAVEL, Status.FINISH, e.Time + e.Car.LoadingTime + e.Car.TravelTime), e.Time + e.Car.LoadingTime + e.Car.TravelTime);
-                    EventQueue.Enqueue(new Event(e.Car ,EventType.UNLOAD, Status.START, e.Time + e.Car.LoadingTime + e.Car.TravelTime), e.Time + e.Car.LoadingTime + e.Car.TravelTime);
-                    EventQueue.Enqueue(new Event(e.Car ,EventType.UNLOAD, Status.FINISH, e.Time + e.Car.LoadingTime + e.Car.TravelTime + e.Car.UnloadingTime), e.Time + e.Car.LoadingTime + e.Car.TravelTime + e.Car.UnloadingTime);
-                    EventQueue.Enqueue(new Event(e.Car ,EventType.TRAVELBACK, Status.START, e.Time + e.Car.LoadingTime + e.Car.TravelTime + e.Car.UnloadingTime), e.Time + e.Car.LoadingTime + e.Car.TravelTime + e.Car.UnloadingTime);
-                    EventQueue.Enqueue(new Event(e.Car ,EventType.TRAVELBACK, Status.FINISH, e.Time + e.Car.LoadingTime + e.Car.TravelTime + e.Car.UnloadingTime + e.Car.TravelTime), e.Time + e.Car.LoadingTime + e.Car.TravelTime + e.Car.UnloadingTime + e.Car.TravelTime);
+                    EventQueue.Enqueue(new Event(e.Car ,EventType.TRAVEL, Status.FINISH, e.Time + e.Car.LoadingTime + e.Car.TravelTime + semaforDelay), e.Time + e.Car.LoadingTime + e.Car.TravelTime + semaforDelay);
+                    EventQueue.Enqueue(new Event(e.Car ,EventType.UNLOAD, Status.START, e.Time + e.Car.LoadingTime + e.Car.TravelTime + semaforDelay), e.Time + e.Car.LoadingTime + e.Car.TravelTime + semaforDelay);
+                    EventQueue.Enqueue(new Event(e.Car ,EventType.UNLOAD, Status.FINISH, e.Time + e.Car.LoadingTime + e.Car.TravelTime + e.Car.UnloadingTime + semaforDelay), e.Time + e.Car.LoadingTime + e.Car.TravelTime + e.Car.UnloadingTime + semaforDelay);
+                    EventQueue.Enqueue(new Event(e.Car ,EventType.TRAVELBACK, Status.START, e.Time + e.Car.LoadingTime + e.Car.TravelTime + e.Car.UnloadingTime + semaforDelay), e.Time + e.Car.LoadingTime + e.Car.TravelTime + e.Car.UnloadingTime + semaforDelay);
+                    EventQueue.Enqueue(new Event(e.Car ,EventType.TRAVELBACK, Status.FINISH, e.Time + e.Car.LoadingTime + e.Car.TravelTime + e.Car.UnloadingTime + e.Car.TravelTime + semaforDelay + semaforDelayZpet), e.Time + e.Car.LoadingTime + e.Car.TravelTime + e.Car.UnloadingTime + e.Car.TravelTime + semaforDelay + semaforDelayZpet);
+                    
                 }
 
                 if (e.EventType == EventType.LOAD && e.Status == Status.FINISH)
@@ -80,9 +124,10 @@
             }
             private void GetInput()
             {
-                Console.WriteLine("Počet ruzných aut a hmotnost pisku:");
+                Console.WriteLine("Počet ruzných aut, hmotnost pisku a interval semaforu:");
                 int[] input = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
                 Sand = input[1];
+                SemaforInterval = input[2];
                 Console.WriteLine($"Na dalších {input[0]} řádků napiš jaké auta chceš ve formátu: count loadingTime unloadingTime travelTime capacity");
 
                 for (int i = 0; i < input[0]; i++)
@@ -98,8 +143,8 @@
             }
         }
 
-        enum Status { START, FINISH}
-        enum EventType { LOAD, UNLOAD, TRAVEL, TRAVELBACK}
+        enum Status { START, FINISH, PRUJEZD, STOP, VYRAZIT}
+        enum EventType { LOAD, UNLOAD, TRAVEL, TRAVELBACK, SEMAFOR}
 
         class Car
         {
