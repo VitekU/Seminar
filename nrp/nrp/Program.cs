99939c42f4383b70cdbe65e38fee09f5fe355c9b
VitekU <ungermann.vit@gmail.com>
2025-03-23 21:53:04 +0100

    nrp

---
+++ nrp/nrp/Program.cs
@@ -0,0 +1,74 @@
+﻿namespace nrp
+{
+    class Program
+    {
+
+        static int[] Solution(int[] s)
+        {
+            int[] t = new int[s.Length + 1];
+            int[] p = new int[s.Length + 1];
+            
+            for (int i = s.Length; i >= 0; i--)
+            {
+                t[i] = 1;
+                p[i] = 0;
+
+                for (int j = i + 1; j < s.Length; j++)
+                {
+                    if (s[i] < s[j] && t[i] < 1 + t[j])
+                    {
+                        t[i] = 1 + t[j];
+                        p[i] = j;
+                    }
+                }
+            }
+
+            int[] nrp = new int[t[0] - 1];
+            int index = p[0];
+            int ptr = 0;
+            while (index != 0)
+            {
+                nrp[ptr] = s[index];
+                index = p[index];
+                ptr++;
+            }
+            
+        
+            return nrp;
+        } 
+        static void Main(string[] args)
+        {
+            using (StreamReader reader = new StreamReader("vstupy.txt"))
+            {
+                string line = reader.ReadLine();
+                while (line != null)
+                {
+                    if (line == string.Empty)
+                    {
+                        Console.WriteLine("Posloupnost je prázdná!");
+                    }
+                    else
+                    {
+                        List<int> i = line.Split().Select(int.Parse).ToList();
+                        i.Insert(0, int.MinValue);
+                        int[] s = i.ToArray();
+
+                        foreach (var e in Solution(s))
+                        {
+                            Console.Write(e + " ");
+                        }
+
+                        Console.WriteLine();
+                    
+                        //Console.WriteLine(line);
+                    }
+                    
+                    reader.ReadLine();
+                    line = reader.ReadLine();
+                    
+
+                }
+            }
+        }
+    }
+}
